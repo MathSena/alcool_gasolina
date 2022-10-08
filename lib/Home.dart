8e55adf3cf21eeeebf31130b027d887b940ee8cc
Matheus Sena <MathSena07@hotmail.com>
2022-10-07 23:06:03 -0300

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController _controllerAlcool = TextEditingController();
+  final TextEditingController _controllerGasolina = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Álcool ou gasolina"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: Image.asset("images/logo.png"),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecer seu carro",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration:
+                    const InputDecoration(labelText: "Preço Alcool, ex: 1.59"),
+                style: const TextStyle(fontSize: 22),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: "Preço Gasolina, ex: 3.59"),
+                style: const TextStyle(fontSize: 22),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.blueAccent),
+                      padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
+                    ),
+                    child: const Text(
+                      "Calcular",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )),
+              const Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
